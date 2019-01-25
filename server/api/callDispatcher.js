106c63e411c29db679d9cebbac1a606ba9fc5fc8
Paz <Owner@DESKTOP-AVO46NR>
2019-01-25 13:53:04 +0200

    Cleaned up call dispatcher - add some doc, removed debugging console prints and set a meaningfull error message.

--- server/api/callDispatcher.js
+++ server/api/callDispatcher.js
@@ -3,32 +3,27 @@ const {generateOriginateCallAction} = require('../asteriskActionGenerators/callA
     
 const placeCall = (number , extension) => {
   var ami = new require('asterisk-manager')(amiPort, amiServer, amiUser, amiPassword, true);
-  // ami.on('queuemember' , function (event) {
-  console.log('Started.');
-  
   // In case of any connectiviy problems we got you coverd.
   ami.keepConnected();
-  
   // Listen for any/all AMI events.
   ami.on('managerevent', function(evt) {
     console.log(evt.event);
-    
   });
 
-
-
-
+  //Handle information needed to orginiate a call - note that the extension assumes SIP as interface.
   const numToCall = Number.parseInt(number) ? number : 101;
   const extensionString = `SIP/${extension}`;
 
+  //Generate action object and pass it to ami api. 
   ami.action(generateOriginateCallAction(extensionString , undefined, numToCall), function(err, res) {
     if(err) {
-      console.log('An error has occured! ' , err);
+      //Print an error that will indicate what went wrong for the programmer.
+      console.log(`Error - Unable to originate call from ${extensionString} to ${numToCall} -- placeCall()` , err);
     }
   });
 }
 
-
+//Module exports:
 module.exports = {
   placeCall
 };
